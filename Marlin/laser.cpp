c4f459f50ee184194ecffdfa0483cc72b1ea107a
Timothy Schmidt <timschmidt@gmail.com>
2013-09-26 14:35:30 -0400

    first commit

---
+++ Marlin/laser.cpp
@@ -0,0 +1,24 @@
+/*
+  laser.cpp - Laser control library for Arduino using 16 bit timers- Version 1
+  Copyright (c) 2013 Timothy Schmidt.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "Configuration.h" 
+#include <avr/interrupt.h>
+#include <Arduino.h>
+
+
